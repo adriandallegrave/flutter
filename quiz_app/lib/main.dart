39532e4c59c2cdecf6750413140580fdfc991060
adriandallegrave <adriandallegrave@gmail.com>
2022-02-06 21:41:55 -0300

    quiz02 - feat: column, button, anonymous-functions

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -5,14 +6,48 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void answerQuestion() {
+    if (kDebugMode) {
+      print('Answer chosen!');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+    ];
+    questions.add('What\'s your favorite animarl?');
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My first App'),
         ),
-        body: const Text('This is my default text!'),
+        body: Column(
+          children: [
+            const Text('The question!'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              child: const Text('Answer 2'),
+              onPressed: () {
+                if (kDebugMode) {
+                  print('Answer 2 chosen');
+                }
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Answer 3'),
+              onPressed: () {
+                if (kDebugMode) {
+                  print('Answer 3 chosen');
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
