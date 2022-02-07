e1b373e4c59168c0d2b22bcf2df812a1e58f9f62
adriandallegrave <adriandallegrave@gmail.com>
2022-02-06 23:43:54 -0300

    quiz10 - feat: finished

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -9,7 +9,7 @@ class Quiz extends StatelessWidget {
 
   final List<Map<String, Object>> questions;
   final int questionIndex;
-  final VoidCallback answerQuestion;
+  final Function answerQuestion;
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +18,10 @@ class Quiz extends StatelessWidget {
         Question(
           questions[questionIndex]['questionText'] as String,
         ),
-        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(answerQuestion, answer);
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(
+              () => answerQuestion(answer['score']), answer['text'] as String);
         }).toList()
       ],
     );
